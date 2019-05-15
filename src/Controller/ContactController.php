385c23d48dbd39717f8676d236b91af7387bb3ce
benurk123 <43404853+benurk123@users.noreply.github.com>
2019-05-15 14:36:48 +0200

    Add files via upload

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Entity\Contact;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\Length;
 
 class ContactController extends  Controller{
     /**
@@ -32,14 +34,13 @@ class ContactController extends  Controller{
 
         $form = $this->createFormBuilder($contact)
             ->add('naam', TextType::class, array('attr' => array('class' => 'form-control')))
-            ->add('email', TextareaType::class, array(
+            ->add('email', EmailType::class, array(
                 'required' => true,
                 'attr' => array('class' => 'form-control')
             ))
-            ->add('telefoonnummer', TextareaType::class, array(
-                'required' => true,
-                'attr' => array('class' => 'form-control')
-            ))
+            ->add('telefoonnummer', TextareaType::class, [
+                'constraints' => new Length(['min' => 10]) , 'attr' => array('class' => 'form-control'),
+            ])
             ->add('save', SubmitType::class, array(
                 'label' => 'Create',
                 'attr' => array('class' => 'btn btn-primary mt-3')
